795f3c8a45ad83b30dc3ae0c0f1acd06b4a691d4
david <davidmarques856@gmail.com>
2020-11-08 17:14:01 +0000

    Finished the web app

--- app.py
+++ app.py
@@ -5,10 +5,27 @@
 
 import dash
 import dash_core_components as dcc
+import pandas as pd 
+import numpy as np
+import plotly.express as px
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.express as px
-import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from functools import reduce
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import LabelBinarizer, OneHotEncoder, RobustScaler
+from sklearn.compose import make_column_transformer
+
+from process_data import *
+
+# Reads the data
+
+df = get_data()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -17,8 +34,26 @@ app.title = 'Term Deposit Marketing Campaing '
 # Create the webapp
 app.layout = html.Div(children=[
     html.H1(children='Term Deposit Marketing Campaing Dashboard'),
+    dcc.Graph(figure=label_counts(df)),
+    dcc.Graph(figure=plot_monthly_success(df)),
+    dcc.Graph(figure=box_plot_cont(df)),
+    dcc.Graph(id="cat-graph"),
+
+    dcc.Dropdown(
+        id='cat-dropdown',
+        options=[{"label": c, "value": c} for c in categorical_features],
+        value='job'
+    ),
+    
+    dcc.Graph(figure=logistic_feature_importance(df)),
+
 ])
 
+@app.callback(
+    Output('cat-graph', 'figure'),
+    [Input('cat-dropdown', 'value')])
+def update_cities(column):
+    return bar_plot_disc_variables(df, column)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ process_data.py
@@ -0,0 +1,122 @@
+import pandas as pd 
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from functools import reduce
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import LabelBinarizer, OneHotEncoder, RobustScaler
+from sklearn.compose import make_column_transformer
+
+categorical_features = ["job", "marital", "education", "default", "housing", "loan", "contact", "poutcome"]
+cols_to_drop = ["y", "month"]
+
+def get_data() -> pd.DataFrame:
+    """ Gets and processes the data """
+
+    df = pd.read_csv("data/data.csv", sep=";")
+    df["y_numeric"] = df["y"].map({"no": 0, "yes": 1})
+    assert not df.isna().any().any() # Assert that there are no missing values
+
+    # Transform variables into categorical type
+    df[categorical_features] = df[categorical_features].astype('category')
+
+    # Encode the months using sin transform
+    df["month_encoded"] = pd.to_datetime(df["month"], format="%b").apply(lambda x: np.sin((x.month/12)*2*np.pi))
+    return df
+
+
+def label_counts(df: pd.DataFrame):
+    """ Plots the label counts """
+    fig = px.bar(df["y"].value_counts(), title=f"Success Rate {int(round(5289/39922, 2)*100)}%") 
+    fig.layout.yaxis.title = "# Customers"
+    fig.layout.xaxis.title = "Successfull contact"
+    fig.layout.showlegend = False
+    return fig
+
+def plot_monthly_success(df: pd.DataFrame):
+    """ Plots the monthly success rate and total number of calls """
+    ordered_months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+    plot_df = df[["month", "y_numeric"]].groupby("month").agg(
+        n_calls=pd.NamedAgg(column="y_numeric", aggfunc=lambda x: x.shape[0]),
+        success_rate=pd.NamedAgg(column="y_numeric", aggfunc=lambda x: (x.sum()/x.shape[0])*100)).loc[ordered_months, :]
+
+    # Create figure with secondary y-axis
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # Add traces
+    fig.add_trace(
+        go.Scatter(x=plot_df.index, y=plot_df["n_calls"], name="# Calls"),
+        secondary_y=False,
+    )
+
+    fig.add_trace(
+        go.Scatter(x=plot_df.index, y=plot_df["success_rate"], name="Success Rate"),
+        secondary_y=True,
+    )
+
+    # Add figure title
+    fig.update_layout(
+        title_text="# Number of calls and Success Rate"
+    )
+
+    # Set x-axis title
+    fig.update_xaxes(title_text="Month")
+
+    # Set y-axes titles
+    fig.update_yaxes(title_text="# Calls", secondary_y=False)
+    fig.update_yaxes(title_text="Success Rate [%]", secondary_y=True)
+
+    return fig
+
+
+def box_plot_cont(df: pd.DataFrame):
+    """ Plots the box plot of the continuous variables """
+    cols = ["balance", "age", "campaign", "previous", "duration"]
+    fig = px.box(df[["y"] + cols].melt(id_vars="y"), x="y", y="value", facet_col="variable")
+    fig.update_yaxes(matches=None)
+
+    # Show the ticks for the graphs
+    for i in range(2, len(cols)+1):
+        fig.update_yaxes(showticklabels=True, col=i) # assuming second facet
+
+    return fig
+
+def bar_plot_disc_variables(df: pd.DataFrame, feature: str):
+    """ Plots the Bar plots for the discrete variables"""
+    fig = px.bar(df[[feature, "y_numeric"]].groupby(feature).agg(
+        TotalCount = pd.NamedAgg(column="y_numeric", aggfunc="count"), 
+        SuccessCount = pd.NamedAgg(column="y_numeric", aggfunc=sum)
+        ).reset_index().melt(id_vars=feature).rename(columns={"value": "Count Value", "variable": "Count Type"}), x=feature, y="Count Value", color="Count Type")
+
+    fig.update_xaxes(matches=None)
+    fig.update_layout(barmode='group')
+    fig.layout.title.text = f"Total and Success Counts of variable {feature}"
+    return fig
+
+def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+    """ Transforms the Data frame """
+    train_data = df.drop(cols_to_drop, axis=1).copy()
+    oh = OneHotEncoder(drop="first")
+    oh.fit(train_data[categorical_features])
+    cat_enc_df = pd.DataFrame(oh.transform(train_data[categorical_features]).toarray(), columns=oh.get_feature_names(categorical_features))
+    return train_data.select_dtypes(exclude="category").join(cat_enc_df)
+
+def logistic_feature_importance(df: pd.DataFrame):
+    """ Fit Logistic Regression and plots the feature importance """
+    train_data = transform_data(df)
+
+    label = "y_numeric"
+    X = train_data.drop(label, axis=1)
+    y = df[label]
+
+    mean_cross_val_score = cross_val_score(LogisticRegression(), X, y, cv=StratifiedKFold(5), scoring="f1").mean()
+    clf = LogisticRegression()
+    clf.fit(X, y)
+    fig = px.bar(pd.Series(clf.coef_.flatten(), index=X.columns).sort_values(), title=f"Logistic regression feature importance. 5 Fold Cross Validation F1 score: {round(mean_cross_val_score, 2)}")
+    fig.layout.showlegend = False
+    return fig

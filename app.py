88728d61afa7be856780ff1f902dfb37d346a2e8
david <davidmarques856@gmail.com>
2020-11-08 19:52:30 +0000

    Finished the Docker Image

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def update_cities(column):
     return bar_plot_disc_variables(df, column)
 
 if __name__ == '__main__':
-    app.run_server(debug=False, port=8050)
+    app.run_server(debug=False, port=8050,  host='0.0.0.0')
